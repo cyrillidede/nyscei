788b165ad456953fb58c1b4716ff67489b6f8c6e
cyrillidede <64872286+cyrillidede@users.noreply.github.com>
2026-03-19 22:19:33 +0300

    Add WhiteNoise middleware and configure STATIC_ROOT
    Added WhiteNoise middleware for static file handling and configured STATIC_ROOT.

--- ebenezersite/settings.py
+++ ebenezersite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ebenezersite.urls'
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR /"static",
 ]
