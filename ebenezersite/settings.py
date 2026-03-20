0bf8839c651c6e4c95597a2c217b3d0fd4c189e6
cyrillidede <64872286+cyrillidede@users.noreply.github.com>
2026-03-20 14:47:08 +0300

    Update settings.py

--- ebenezersite/settings.py
+++ ebenezersite/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'ebenezersite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
+DATABASES['default']['OPTIONS'] = {
+    'sslmode': 'require'
 }
 
 
