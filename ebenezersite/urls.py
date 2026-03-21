ee5cac84fd6a4629526215f54f7c47fe78e7e4aa
cyrillidede <64872286+cyrillidede@users.noreply.github.com>
2026-03-21 15:16:20 +0300

    Add activities path to URLs

--- ebenezersite/urls.py
+++ ebenezersite/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('account', views.account, name='account'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('activities/', views.activities, name = 'activities')
     path('', include('accounts.urls')),
 ]
