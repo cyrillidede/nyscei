0970fab14d8a7bb7fcceefcb4d6bf3361295a75c
cyrillidede <64872286+cyrillidede@users.noreply.github.com>
2026-03-25 16:45:08 +0300

    Update views.py

--- ebenezersite/views.py
+++ ebenezersite/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from main.models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-from .forms import UserRegistrationForm, ProfileForm
+
 
 
 
@@ -32,23 +32,4 @@ def account(request):
     profile = request.user.profile
     return render(request, 'account.html', {'profile': profile})
     
-def signup(request):
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        profile_form = ProfileForm(request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save(commit=False)
-            user.set_password(user_form.cleaned_data['password'])
-            user.save()
-            
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-            
-            login(request, user)
-            return redirect('account_page')
-    else:
-        user_form = UserRegistrationForm()
-        profile_form = ProfileForm()
-    return render(request, 'signup.html', {'user_form': user_form, 'profile_form': profile_form})
 
