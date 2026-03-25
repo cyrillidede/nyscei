73b52d82a520f732a6e62f1d666fde894a4ef75b
cyrillidede <64872286+cyrillidede@users.noreply.github.com>
2026-03-25 16:09:21 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,16 @@ from django.shortcuts import render, redirect
 @login_required
 def account(request):
      return render(request, 'accounts/account.html', {'user': request.user})
+
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # or your account page
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
