850f0c7b25e1056dfee307f932b1c976334c2125
cyrillidede <64872286+cyrillidede@users.noreply.github.com>
2026-03-25 07:44:51 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .forms import UserUpdateForm
+
 
 @login_required
 def account(request):
