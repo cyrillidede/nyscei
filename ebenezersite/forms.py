081666980af6dd55ae49b71f597a434ca5cbef4c
cyrillidede <64872286+cyrillidede@users.noreply.github.com>
2026-03-25 15:59:03 +0300

    Update forms.py

--- ebenezersite/forms.py
+++ ebenezersite/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from account.models import Profile
+from accounts.models import Profile
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
