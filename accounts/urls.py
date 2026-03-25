3958c92965460908b73ea09b00c23d95bee42225
cyrillidede <64872286+cyrillidede@users.noreply.github.com>
2026-03-25 17:01:57 +0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('account', views.account, name='account'),
-    path('signup', views.signup, name = 'signup',
+    path('signup', views.signup, name = 'signup'),
 ]
